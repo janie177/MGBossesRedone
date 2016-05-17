9b044ad67ad1914f1ff8791b72c27483d6118fd1
Jan <janie177@hotmail.com>
2016-05-17 13:32:02 +0200

    Check if world is loaded at all.

--- src/main/java/com/minegusta/mgbossesredone/listeners/SpawnListener.java
+++ src/main/java/com/minegusta/mgbossesredone/listeners/SpawnListener.java
@@ -18,11 +18,12 @@ public class SpawnListener implements Listener
         for(SpawnLocation l : LocationRegistry.getLocations())
         {
             if(l.getIfSpawned()) continue;
+            if(Bukkit.getWorld(l.getLocation().getWorld().toString()) == null) continue;
             Chunk chunk = l.getLocation().getChunk();
             Chunk eventChunk = e.getChunk();
             if(chunk.getX() == eventChunk.getX() && chunk.getZ() == eventChunk.getZ())
             {
-                Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(), ()-> l.getBoss().spawnBoss(l), 40);
+                l.getBoss().spawnBoss(l);
             }
         }
     }
